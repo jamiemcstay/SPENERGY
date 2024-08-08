291651c02a4029cb1641b52bd79d2dcf1275fd3b
jamiemcstay <83792601+jamiemcstay@users.noreply.github.com>
2024-08-08 15:49:25 +0000

    Add redirectTOSEAI functino in js file to open link to SEAI site in new page

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+function redirectToSEAI() {
+    var url = 'https://www.seai.ie/grants/business-grants/' +
+              'commercial-solar-pv/';
+    window.open(url, '_blank');
+}
+
+// DOMContentLoaded event listener if you have other functionality
 document.addEventListener('DOMContentLoaded', function() {
     var menuToggle = document.getElementById('menuToggle');
     var menu = document.getElementById('menu');
 
-    menuToggle.addEventListener('click', function() {
-        menu.classList.toggle('active');
-    });
+    if (menuToggle && menu) {
+        menuToggle.addEventListener('click', function() {
+            menu.classList.toggle('active');
+        });
+    }
 });
